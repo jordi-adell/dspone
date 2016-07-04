d4e2c9835082eb112e392419fc4a30ee0c75d997
Jordi Adell <jadell@verbio.com>
2016-07-04 08:03:48 +0200

    SigalProcessor converter into a higher level interface
    on top of shorttimeprocess. This higher level interface is intended
    for external libraries that only need to know that the modules
    they are calling process signal buffer, and not any other deatil.

--- include/dspone/rt/SignalProcessor.h
+++ include/dspone/rt/SignalProcessor.h
@@ -19,123 +19,50 @@
 * You should have received a copy of the GNU General Public License
 * along with DSPONE.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef __ISHORTTIMEPROCESS_H_
-#define __ISHORTTIMEPROCESS_H_
+#ifndef __SIGNAL_PROCESSOR_H_
+#define __SIGNAL_PROCESSOR_H_
 
-#include <dspone/rt/ProcessDispatcher.h>
 #include <vector>
+#include <stdint.h>
+
+#include <boost/shared_array.hpp>
 
 namespace dsp {
 
-class ShortTimeProcess;
-class ShortTimeAnalysis;
 
-/**
-	 * @brief The IShortTimeProcess class
-	 * Interface class to expose the API for classes based on ShortTimeProcess
-	 * ShortTimeProcess is the Overlap and Add process of audio signals for
-	 * realtime audio processing.
-	 */
-class SignalProcessor : public ProcessDispatcher<SignalProcessor>
+class SignalProcessor
 {
     public:
+	SignalProcessor() {}
+	virtual ~SignalProcessor() {}
 
-	SignalProcessor();
-	virtual ~SignalProcessor();
+	virtual int process(const std::vector<double*> &signal,
+		    unsigned int inbuffersize,
+		    const std::vector<double*> &output,
+		    unsigned int outbuffersize) = 0;
 
-	/**
-	   * @brief process   Process an input signal as decribed in @class SignalProcessingDispatcher
-	   * for int16_t data type.
-	   */
-	int process(const std::vector<int16_t *> &signal,
+	virtual int process(const std::vector<float*> &signal,
 		    unsigned int inbuffersize,
-		    const std::vector<int16_t *> &output,
-		    unsigned int outbuffersize);
-	int process(const std::vector<std::shared_ptr<int16_t> > &signal,
+		    const std::vector<float*> &output,
+		    unsigned int outbuffersize) = 0;
+
+	virtual int process(const std::vector<int16_t*> &signal,
 		    unsigned int inbuffersize,
-		    const std::vector<std::shared_ptr<int16_t> > &output,
-		    unsigned int outbuffersize);
-	/**
-	   * @brief process   Process an input signal as decribed in @class SignalProcessingDispatcher
-	   * for float data type.
-	   */
-	int process(const std::vector<float *> &signal,
+		    const std::vector<int16_t*> &output,
+		    unsigned int outbuffersize) = 0;
+
+	virtual int process(const std::vector<uint16_t*> &signal,
 		    unsigned int inbuffersize,
-		    const std::vector<float *> &output,
-		    unsigned int outbuffersize);
-	int process(const std::vector<std::shared_ptr<float> > &signal,
-		    unsigned int inbuffersize,
-		    const std::vector<std::shared_ptr<float> > &output,
-		    unsigned int outbuffersize);
-	/**
-	   * @brief process   Process an input signal as decribed in @class SignalProcessingDispatcher
-	   * for double data type.
-	   */
-	int process(const std::vector<double *> &signal,
-		    unsigned int inbuffersize,
-		    const std::vector<double *> &output,
-		    unsigned int outbuffersize);
-	int process(const std::vector<std::shared_ptr<double> > &signal,
-		    unsigned int inbuffersize,
-		    const std::vector<std::shared_ptr<double> > &output,
-		    unsigned int outbuffersize);
+		    const std::vector<uint16_t*> &output,
+		    unsigned int outbuffersize) = 0;
 
 
+	virtual int getLatency() const = 0;
+	virtual int getMaxLatency() const = 0;
+	virtual int getBufferSize() const = 0;
+	virtual int getNumberOfChannels() const = 0;
 
-	virtual int getLatency() const;
-	virtual int getMaxLatency() const;
-	virtual int getBufferSize() const;
-	virtual int getNumberOfChannels() const;
-
-    protected:
-	std::unique_ptr<ShortTimeProcess> _impl;  /**< Actual implementation of the algorithm for signal processing. */
 };
-
-
-
-/**
-	 * @brief The IShortTimeAnalysis class
-	 * Interface class to expose the API for classes based on ShortTimeAnalysis
-	 * ShortTimeProcess is the Overlap and Add process of audio signals for
-	 * realtime audio processing.
-	 */
-class SignalAnalyser : public ProcessDispatcher<SignalAnalyser>
-{
-    public:
-
-	SignalAnalyser();
-	virtual ~SignalAnalyser();
-
-	/**
-	   * @brief process   Process an input signal as decribed in @class SignalProcessingDispatcher
-	   * for int16_t data type.
-	   */
-	virtual int process(const std::vector<int16_t *> &signal, unsigned int buffersize);
-	virtual int process(const std::vector<std::shared_ptr<int16_t> > &signal, unsigned int buffersize);
-	/**
-	   * @brief process   Process an input signal as decribed in @class SignalProcessingDispatcher
-	   * for float data type.
-	   */
-	virtual int process(const std::vector<float *> &signal, unsigned int buffersize);
-	virtual int process(const std::vector<std::shared_ptr<float> > &signal, unsigned int buffersize);
-	/**
-	   * @brief process   Process an input signal as decribed in @class SignalProcessingDispatcher
-	   * for double data type.
-	   */
-	virtual int process(const std::vector<double *> &signal, unsigned int buffersize);
-	virtual int process(const std::vector<std::shared_ptr<double> > &signal, unsigned int buffersize);
-
-
-	/** Latency managment functions **/
-	virtual int getLatency() const;
-	virtual int getMaxLatency() const;
-	virtual int getBufferSize() const;
-	virtual int getNumberOfChannels() const;
-
-    protected:
-	std::unique_ptr<ShortTimeAnalysis> _impl; /**< Actual implementation of the algorithm for signal analysis. */
-};
-
 
 
 }
